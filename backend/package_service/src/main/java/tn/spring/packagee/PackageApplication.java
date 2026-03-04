d74bfd684c05c69fa4aab27a96f0f04900a24e07
khalilsahnoun123 <sahnoun.khalil78@gmail.com>
2026-03-04 15:27:13 +0100

    init service package maangment

---
+++ backend/package_service/src/main/java/tn/spring/packagee/PackageApplication.java
@@ -0,0 +1,13 @@
+package tn.spring.packagee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageApplication.class, args);
+	}
+
+}
---
+++ backend/package_service/src/test/java/tn/spring/packagee/PackageApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.spring.packagee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
