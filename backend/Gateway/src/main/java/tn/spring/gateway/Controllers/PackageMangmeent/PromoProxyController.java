a4bb93ac1001a4daa92f52ad1c7242edec860fae
khalilsahnoun123 <sahnoun.khalil78@gmail.com>
2026-03-04 20:16:54 +0100

    add neww endpoint to promocontroller

--- backend/Gateway/src/main/java/tn/spring/gateway/Controllers/PackageMangmeent/PromoProxyController.java
+++ backend/Gateway/src/main/java/tn/spring/gateway/Controllers/PackageMangmeent/PromoProxyController.java
@@ -28,4 +28,34 @@ public class PromoProxyController {
     public ResponseEntity<String> validate(@RequestBody Map<String, Object> body, HttpServletRequest req) {
         return proxy.forward(PROMO_BASE + "/validate", HttpMethod.POST, body, req);
     }
+    @GetMapping
+    public ResponseEntity<String> getAll(HttpServletRequest req) {
+        return proxy.forward(PROMO_BASE, HttpMethod.GET, null, req);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getById(@PathVariable Long id, HttpServletRequest req) {
+        return proxy.forward(PROMO_BASE + "/" + id, HttpMethod.GET, null, req);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@PathVariable Long id,
+                                         @RequestBody Map<String, Object> body,
+                                         HttpServletRequest req) {
+        return proxy.forward(PROMO_BASE + "/" + id, HttpMethod.PUT, body, req);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id, HttpServletRequest req) {
+        return proxy.forward(PROMO_BASE + "/" + id, HttpMethod.DELETE, null, req);
+    }
+    @PutMapping("/{id}/enable")
+    public ResponseEntity<String> enable(@PathVariable Long id, HttpServletRequest req) {
+        return proxy.forward(PROMO_BASE + "/" + id + "/enable", HttpMethod.PUT, null, req);
+    }
+
+    @PutMapping("/{id}/disable")
+    public ResponseEntity<String> disable(@PathVariable Long id, HttpServletRequest req) {
+        return proxy.forward(PROMO_BASE + "/" + id + "/disable", HttpMethod.PUT, null, req);
+    }
 }
